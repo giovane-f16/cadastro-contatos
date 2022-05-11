615d25d07b27ccd9c68b023887086349d9a5f821
giovane <giovane_05silva@hotmail.com>
2022-05-11 00:29:14 -0300

    Projeto

--- scripts.js
+++ scripts.js
@@ -1,18 +1,8 @@
+/*
 document.getElementById("editar").addEventListener("click", function(){
     document.getElementById("secaoEditar").style.display = "block";
 });
-/*
 document.getElementById("cancelar").addEventListener("click", function(){
     getElementById("secaoEditar").style.display = "none";
 });
-
-
-/*
-function abrirMenu(){
-    document.getElementById("categorias").style.display = "block";
-    document.getElementById("layer").style.display = "block";
-    document.body.style.overflowY = "hidden";
-}
-document.getElementById("menuicon").addEventListener("click", function(){
-    abrirMenu();
-});*/
+*/
